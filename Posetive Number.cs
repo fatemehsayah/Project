041f14e7c76c904a0b23b3084695b0167bdc6252
Fatemeh <tak.star_75@yahoo.com>
2021-09-03 09:16:25 +0430

    Create Posetive Number.cs
    Posetive Number

---
+++ Posetive Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosetiveNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter N Number: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+			Console.Write("Posetive Number is: ");
+            for (int i = 1; i < n; i++)
+            {
+				if(i%2 != 0)
+				{
+					Console.Write( i + "\t");
+				}
+            }
+            Console.ReadKey();
+        }
+    }
+}
