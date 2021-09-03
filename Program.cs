c6252a744af0123a616de949e7dd2d69826a5375
Fatemeh <tak.star_75@yahoo.com>
2021-09-03 14:32:12 +0430

    Math Pow

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace reverse1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            Console.WriteLine();
+            for (int i = (n.Length-1); i >=0 ; i--)
+            {
+                Console.Write(n.Substring(i, 1));
+            }
+            
+        }
+    }
+}
---
+++ pow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pow3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int [] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+              
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i]+"\t\t");
+
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                Double x = Math.Pow( arr[i],3);
+                Console.Write(x +"\t\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
