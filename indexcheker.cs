7338d8a053c92f19c9c7e0c48105661b484ba798
Fatemeh <tak.star_75@yahoo.com>
2021-09-03 16:57:28 +0430

    indexcheker
    This reverts commit dcacddf3785044cd9014fcc80ba987b482601815.

---
+++ indexcheker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace indexcheker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+              
+            }
+            int index = int.Parse(Console.ReadLine());
+            int x = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == index)
+                {
+
+                    x = 1;
+                }
+                
+            }
+            if (x==1)
+            {
+                Console.WriteLine("index");
+            }
+            else
+            {
+                Console.WriteLine("no index");
+            }
+            Console.ReadKey();
+        }
+    }
+}
